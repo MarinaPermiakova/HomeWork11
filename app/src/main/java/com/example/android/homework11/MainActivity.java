57b3e2ddf0d3fcd25cd20928ecb507e07325937e
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-07-02 18:14:20 +0400

    second commit

--- app/src/main/java/com/example/android/homework11/MainActivity.java
+++ app/src/main/java/com/example/android/homework11/MainActivity.java
@@ -3,12 +3,118 @@ package com.example.android.homework11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String number = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.text_view);
+        Button button0 = findViewById(R.id.btn0);
+        Button button1 = findViewById(R.id.btn1);
+        Button button2 = findViewById(R.id.btn2);
+        Button button3 = findViewById(R.id.btn3);
+        Button button4 = findViewById(R.id.btn4);
+        Button button5 = findViewById(R.id.btn5);
+        Button button6 = findViewById(R.id.btn6);
+        Button button7 = findViewById(R.id.btn7);
+        Button button8 = findViewById(R.id.btn8);
+        Button button9 = findViewById(R.id.btn9);
+        Button buttonComma = findViewById(R.id.commaBtn);
+        
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._0));
+                textView.setText(number);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._1));
+                textView.setText(number);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._2));
+                textView.setText(number);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._3));
+                textView.setText(number);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._4));
+                textView.setText(number);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._5));
+                textView.setText(number);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._6));
+                textView.setText(number);
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._7));
+                textView.setText(number);
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._8));
+                textView.setText(number);
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string._9));
+                textView.setText(number);
+            }
+        });
+
+        buttonComma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number = number.concat(getString(R.string.comma));
+                textView.setText(number);
+            }
+        });
     }
 }
